f61c6def09d51953c045b4c242677b59d27630ed
szymon cieszynski <cieszynski8@gmail.com>
2023-05-19 10:20:08 +0200

    Clear whole cart event listener

--- src/Form/CartType.php
+++ src/Form/CartType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Order;
+use App\Form\EventListener\ClearCartListener;
 use App\Form\EventListener\RemoveCartItemListener;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\AbstractType;
@@ -22,6 +23,7 @@ class CartType extends AbstractType
             ->add('clear', SubmitType::class);
 
         $builder->addEventSubscriber(new RemoveCartItemListener());
+        $builder->addEventSubscriber(new ClearCartListener());
     }
 
     public function configureOptions(OptionsResolver $resolver): void
