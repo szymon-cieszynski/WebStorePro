08a7aec50b2ddae539e4ce64d357d3df2f571aaa
szymon cieszynski <cieszynski8@gmail.com>
2023-06-18 13:30:27 +0200

    Unit test for User Entity

---
+++ tests/UserTest.php
@@ -0,0 +1,53 @@
+<?php
+
+use App\Entity\Order;
+use App\Entity\User;
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    public function testGetEmail()
+    {
+        $user = new User();
+        $user->setEmail('sklep@example.com');
+
+        $email = $user->getEmail();
+
+        $this->assertEquals('sklep@example.com', $email);
+    }
+
+    public function testAddOrder(): void
+    {
+        $user = new User();
+        $order = new Order();
+
+        $user->addOrder($order);
+
+        //check if collection of orders and user contains exact 1 element
+        $this->assertCount(1, $user->getOrders());
+        //check if user is the same user assigned to order
+        $this->assertSame($user, $order->getUser());
+
+        //check if duplicates are ignored
+        $user->addOrder($order);
+        $this->assertCount(1, $user->getOrders());
+
+        //check if objects are detached correctly
+        $user->removeOrder($order);
+        $this->assertCount(0, $user->getOrders()); //if collection will conatin object, removeOrder didn't works properly
+        $this->assertNull($order->getUser());
+    }
+
+    public function testRemoveOrder(): void
+    {
+        $user = new User();
+        $order = new Order();
+
+        $user->removeOrder($order);
+
+        //check if collection of orders is empty
+        $this->assertCount(0, $user->getOrders());
+
+        $this->assertNull($order->getUser());
+    }
+}
